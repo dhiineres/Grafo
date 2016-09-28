601b61be198534dfa6e1a0224f7666df15f5e80b
Dhiineres <dhiogoneres@gmail.com>
2016-09-28 14:12:51 -0300

    aliás, onde que envia isso?? ;-;

--- TrabGrafo/src/br/ufg/inf/es/grafos/Grafo.java
+++ TrabGrafo/src/br/ufg/inf/es/grafos/Grafo.java
@@ -4,77 +4,76 @@ import java.awt.List;
 import java.util.ArrayList;
 
 public class Grafo {
-    
+
     private int numVertices;
     private ArrayList<Boolean> arestas;
     private ArrayList<Integer> ordVertices;
     private ArrayList<Integer> grauVertices;
-    
-    
-    public Grafo(int size){
-        if (size > 0){
+
+    public Grafo(int size) {
+        if (size > 0) {
             this.numVertices = size;
             this.arestas = new ArrayList();
             this.ordVertices = new ArrayList();
             this.grauVertices = new ArrayList();
         }
     }
-    
-    public int getSize(){
+
+    public int getSize() {
         return this.numVertices;
     }
-    
-    public void addAresta(Boolean is){
+
+    public void addAresta(Boolean is) {
         this.arestas.add(is);
     }
-    
-    public int getIndex(int i, int j){
-        if (i > this.numVertices || i <= 0 || j > this.numVertices || j <= 0){
+
+    public int getIndex(int i, int j) {
+        if (i > this.numVertices || i <= 0 || j > this.numVertices || j <= 0) {
             return -1;
-        }else{
-            if (i > j){
+        } else {
+            if (i > j) {
                 return (((i * i - i) / 2) + j - 1);
             }
             return (((j * j - j) / 2) + i - 1);
         }
     }
-    
-    public int getGrau(int i){
+
+    public int getGrau(int i) {
         int grau = 0;
-        for (int j = 1; j <= this.numVertices; j++){
-            if (this.arestas.get(getIndex(i, j))){
+        for (int j = 1; j <= this.numVertices; j++) {
+            if (this.arestas.get(getIndex(i, j))) {
                 grau++;
             }
         }
         return grau;
     }
-   
-    public String getVizinhos(int v){
+
+    public String getVizinhos(int v) {
         StringBuilder out = new StringBuilder();
-        out.append("Vértices vizinhos a "+ v +":\n");
-        for (int i = 1; i <= this.numVertices; i++){
-            if (i != v && this.arestas.get(getIndex(i, v))){
-                out.append(" - "+ i +"\n");
+        out.append("Vértices vizinhos a " + v + ":\n");
+        for (int i = 1; i <= this.numVertices; i++) {
+            if (i != v && this.arestas.get(getIndex(i, v))) {
+                out.append(" - " + i + "\n");
             }
         }
-        
+
         return out.toString();
     }
-    
-    public void setOrdemGraus(){
-        for(int i = 1; i <= this.numVertices; i++){
+
+    public void setOrdemGraus() {
+        for (int i = 1; i <= this.numVertices; i++) {
             this.ordVertices.add(i);
             this.grauVertices.add(getGrau(i));
         }
-        
+
         int j;
         boolean flag = true;
         int auxGrau, auxVertice;
-        
-        while(flag){
+
+        while (flag) {
             flag = false;
-            for (j = 0; j < this.numVertices - 1; j++){
-                if (this.grauVertices.get(j) < this.grauVertices.get(j + 1)){
+            for (j = 0; j < this.numVertices - 1; j++) {
+                if (this.grauVertices.get(j) < this.grauVertices.get(j + 1)) {
                     auxGrau = this.grauVertices.get(j);
                     auxVertice = this.ordVertices.get(j);
                     this.ordVertices.set(j, this.ordVertices.get(j + 1));
@@ -86,101 +85,177 @@ public class Grafo {
             }
         }
     }
-    
-    public String getOrdemGraus(){
+
+    public String getOrdemGraus() {
         StringBuilder out = new StringBuilder();
         out.append("\tV - d\n");
-        for(int i = 0; i < this.numVertices; i++){
-            out.append("\t"+ this.ordVertices.get(i)+" - ");
-            out.append( this.grauVertices.get(i) +"\n");
+        for (int i = 0; i < this.numVertices; i++) {
+            out.append("\t" + this.ordVertices.get(i) + " - ");
+            out.append(this.grauVertices.get(i) + "\n");
         }
         return out.toString();
     }
-    
-    public String getVerticeMaior(){
+
+    public String getVerticeMaior() {
         StringBuilder out = new StringBuilder();
         out.append("Vértice de Maior Grau:");
-        out.append("\nV - "+ this.ordVertices.get(0));
-        out.append("\nd(V) - "+ this.grauVertices.get(0));
+        out.append("\nV - " + this.ordVertices.get(0));
+        out.append("\nd(V) - " + this.grauVertices.get(0));
         return out.toString();
     }
-    
-    public String getVerticeMenor(){
+
+    public String getVerticeMenor() {
         StringBuilder out = new StringBuilder();
         out.append("Vértice de Menor Grau:");
-        out.append("\nV - "+ this.ordVertices.get(this.numVertices - 1));
-        out.append("\nd(V) - "+ this.grauVertices.get(this.numVertices - 1));
+        out.append("\nV - " + this.ordVertices.get(this.numVertices - 1));
+        out.append("\nd(V) - " + this.grauVertices.get(this.numVertices - 1));
         return out.toString();
     }
-    
-    public boolean isCompleto(){
-        for (int i = 0; i < this.numVertices; i++){
-            if (this.grauVertices.get(i) < this.numVertices - 1){
+
+    public boolean isCompleto() {
+        for (int i = 0; i < this.numVertices; i++) {
+            if (this.grauVertices.get(i) < this.numVertices - 1) {
                 return false;
             }
         }
         return true;
     }
-    
-    public boolean isCiclo(){
-        for (int i = 0; i < this.numVertices; i++){
-            if (this.grauVertices.get(i) != 2){
+
+    public boolean isCiclo() {
+        for (int i = 0; i < this.numVertices; i++) {
+            if (this.grauVertices.get(i) != 2) {
                 return false;
             }
         }
         return true;
     }
-    
-    public String printListas(){
+
+    public String printListas() {
         StringBuilder out = new StringBuilder();
         out.append("X 1 2 3 4\n");
-        for(int i = 1; i <= this.numVertices; i++){
-            out.append(i +" ");
-            for (int j = 1; j <= this.numVertices; j++){
-                if(arestas.get(getIndex(i, j))){
+        for (int i = 1; i <= this.numVertices; i++) {
+            out.append(i + " ");
+            for (int j = 1; j <= this.numVertices; j++) {
+                if (arestas.get(getIndex(i, j))) {
                     out.append("° ");
-                }else{
+                } else {
                     out.append("- ");
                 }
             }
             out.append("\n");
         }
-        
-        for (int i = 0; i < arestas.size(); i++){
-            if(arestas.get(i)){
+
+        for (int i = 0; i < arestas.size(); i++) {
+            if (arestas.get(i)) {
                 out.append("T ");
-            }else{
+            } else {
                 out.append("F ");
             }
         }
-        
+
         return out.toString();
     }
-    
-    public boolean isKRegular(int k){
-        for (int i = 0; i < this.numVertices; i++){
-            if (this.grauVertices.get(i) != k){
+
+    public boolean isKRegular(int k) {
+        for (int i = 0; i < this.numVertices; i++) {
+            if (this.grauVertices.get(i) != k) {
                 return false;
             }
         }
         return true;
     }
-    
-    public boolean isCaminho(){
+
+    public boolean isCaminho() {
         int ponta = 0, meio = 0;
-        for (int i = 0; i < this.numVertices; i++){
-            if (this.grauVertices.get(i) == 2){
-                meio ++;
-            }else if (this.grauVertices.get(i) == 1){
-                ponta ++;
-            }else{
+        for (int i = 0; i < this.numVertices; i++) {
+            if (this.grauVertices.get(i) == 2) {
+                meio++;
+            } else if (this.grauVertices.get(i) == 1) {
+                ponta++;
+            } else {
                 return false;
             }
         }
-        if (ponta == 2){
+        if (ponta == 2) {
             return true;
-        }else{
+        } else {
             return false;
         }
+    }
+
+    
+    public int getVerticesComVizinhos() {
+        int out = 0;
+
+        for (int i = 1; i <= this.numVertices; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (arestas.get(getIndex(i, j))) {
+                    out++;
+                }
+            }
+        }
+
+        return out;
+    }
+
+    public ArrayList<Integer> getPontas() {
+        ArrayList<Integer> out = new ArrayList();
+
+        for (int i = 0; i < this.numVertices; i++) {
+            if (grauVertices.get(i) == 1) {
+                out.add(ordVertices.get(i));
+            }
+        }
+        return out;
+    }
+
+    public void RemovePontas(ArrayList<Integer> in, ArrayList<Integer> out) {
+        for (int i = 0; i < in.size(); i++) {
+            int v = in.get(i);
+            int index = out.indexOf(v);
+            out.remove(index);
+            for (int j = 0; j < numVertices; j++) {
+                if (arestas.get(getIndex(v, j))) {
+                    arestas.set(getIndex(v, j), false);
+                }
+            }
+        }
+    }
+
+    
+    public String getCentro() {
+        ArrayList<Integer> out = new ArrayList();
+        Grafo aux = this;
+        for (int i = 1; i <= aux.numVertices; i++){
+            out.add(i);
+        }
+        int numVertLig = aux.getVerticesComVizinhos();
+        int numPontas = aux.getPontas().size();
+        if (numPontas <= 1) {
+            out.add(-1);
+            return "Grafo não é uma árvore";
+        } else {
+            do {
+                ArrayList<Integer> pontas = aux.getPontas();
+                aux.RemovePontas(pontas, out);
+                numPontas = aux.getPontas().size();
+                numVertLig = aux.getVerticesComVizinhos();
+            } while (numPontas > 1 && out.size() > 2);
+        }
+        if (out.size() > 2)
+            return "Grafo não é uma árvore";
+        
+        
+        StringBuilder centro = new StringBuilder();
+        if (out.size() == 2){
+            centro.append("Centros do grafo:\n");
+            for(int i : out)
+                centro.append("- "+ i+"\n");
+        
+        }else{
+            centro.append("Centro do grafo: "+ out.get(0));
+        }
+        
+        return centro.toString();
     }
 }
--- TrabGrafo/src/br/ufg/inf/es/grafos/Main.java
+++ TrabGrafo/src/br/ufg/inf/es/grafos/Main.java
@@ -27,27 +27,92 @@ public class Main {
         grafo.setOrdemGraus();
     }
     
+    public static int menu(){
+        System.out.println("--------------MENU--------------");
+        System.out.println("|1. Determinar Grau            |");
+        System.out.println("|2. Graus Máximo e Mínimo      |");
+        System.out.println("|3. Determinar Vizinhos        |");
+        System.out.println("|4. Obter sequencia de graus   |");
+        System.out.println("|5. Determinar classe          |");
+        System.out.println("|6. Obter k-regularidade       |");
+        System.out.println("|7. Obter centro               |");
+        System.out.println("|0. Sair                       |");
+        System.out.println("--------------------------------\n");
+        
+        return Integer.parseInt(input.nextLine());
+    }
+    
+    public static void printGrau(){
+        System.out.print("Entre com o vértice: ");
+        int v = Integer.parseInt(input.nextLine());
+        System.out.println("Grau de "+ v +": "+ grafo.getGrau(v));
+    }
+    
+    public static void printVizinhos(){
+        System.out.print("Entre com o vértice: ");
+        int v = Integer.parseInt(input.nextLine());
+        System.out.println(grafo.getVizinhos(v));
+    }
+    
+    public static void printClasse(){
+        boolean flag = false;
+        if (grafo.isCaminho()){
+            System.out.println("O grafo é um caminho.");
+            flag = true;
+        }if (grafo.isCiclo()){
+            System.out.println("O grafo é um ciclo.");
+            flag = true;
+        }if (grafo.isCompleto()){
+            System.out.println("O grafo é completo.");
+            flag = true;
+        }
+        if (!flag){
+            System.out.println("O grafo não pertence a nenhuma classe");
+        }
+    }
+    
+    public static void printKRegular(){
+        System.out.print("Entre com k: ");
+        int k = Integer.parseInt(input.nextLine());
+        if(grafo.isKRegular(k)){
+            System.out.println("O grafo é K-Regular de "+ k);
+        }else{
+            System.out.println("O grago não é k-Regular de "+ k);
+        }
+    }
+    
     public static void main(String[] args){
         iniciaGrafo();
-        
-        System.out.println(grafo.printListas());
-        
-        System.out.println(grafo.getOrdemGraus());
-        System.out.println(grafo.getVerticeMaior());
-        System.out.println(grafo.getVerticeMenor());
-        for(int i = 1; i <= grafo.getSize(); i++){
-            System.out.println(grafo.getVizinhos(i));
-        }
-        
-        if (!grafo.isCaminho()){
-            System.out.print("Não ");
-        }System.out.println("é caminho");
-        if(!grafo.isCiclo()){
-            System.out.print("Não ");
-        }System.out.println("é ciclo");
-        if(!grafo.isCompleto()){
-            System.out.print("Não ");
-        }System.out.println("é completo");
-        
+        boolean flag = true;
+        do{
+            System.out.println("\n");
+            switch (menu()){
+                case 1:
+                    printGrau();
+                    break;
+                case 2:
+                    System.out.println(grafo.getVerticeMaior());
+                    System.out.println(grafo.getVerticeMenor());
+                    break;
+                case 3:
+                    printVizinhos();
+                    break;
+                case 4:
+                    System.out.println(grafo.getOrdemGraus());
+                    break;
+                case 5:
+                    printClasse();
+                    break;
+                case 6:
+                    printKRegular();
+                    break;
+                case 7:
+                    //Não funcionou ;-;
+                    break;
+                case 0:
+                    flag = false;
+                    break;
+            } 
+        }while (flag);  
     }
 }
